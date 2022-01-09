08926fd409b93ba9e6ba35f046c1ece64d5f0158
loganworld <akp707d@gmail.com>
2022-01-10 03:19:23 +1000

    fix

--- src/services/http-common.js
+++ src/services/http-common.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const BASE_URL = 'http://localhost:3002';
 
 export default axios.create({
-    baseURL:BASE_URL,
     method: "POST",
     headers: {
         "Content-type": "application/json"
